bd1d93c00d92d480d97d002091115258140d8e35
carlostorres2305 <carlosadm15@hotmail.com>
2024-05-22 16:16:52 -0300

    aula03

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'carlos'
+  const newName = name.toUpperCase();
+  const url = 'https://via.placeholder.com/150'
+
+  function sum(a,b){
+    return a + b
+  }
   return (
     <div className="App">
       <h1>Olá React</h1>
-      <p>Curso react</p>
+      <p>Olá {newName}</p>
+      <p>Soma: {sum(2,3)}</p>
+      <img src={url} alt="minha Imagem" />
     </div>
   );
 }
