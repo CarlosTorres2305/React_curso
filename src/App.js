df01f2ff1412a4b9397c87f8f7c891c196b0d25f
carlostorres2305 <carlosadm15@hotmail.com>
2024-05-23 16:11:47 -0300

    dia 02

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Pessoa from './components/Pessoa.js'
+import Frase from './components/Frase.js'
+import Lista from './components/Lista.js'
 function App() {
-  const name = 'carlos'
-  const newName = name.toUpperCase();
-  const url = 'https://via.placeholder.com/150'
-
-  function sum(a,b){
-    return a + b
-  }
+  
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <p>Olá {newName}</p>
-      <p>Soma: {sum(2,3)}</p>
-      <img src={url} alt="minha Imagem" />
+      <h1>Testando</h1>
+      <Frase />
+      <Frase/>
+      <Pessoa 
+      nome="Carlos" 
+      idade="15"
+      profissao="Analista.TI"
+      foto="https://via.placeholder.com/150" />
+      <Lista />
     </div>
   );
 }
---
+++ src/components/SayMyName.js
@@ -0,0 +1,7 @@
+function SayMyName(props){
+    return(
+        <p>Fala aí, {props.nome}, Beleza? </p>
+    )
+}
+
+export default SayMyName;
